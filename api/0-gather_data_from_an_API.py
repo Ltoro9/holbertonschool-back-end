8f906d44233d82ec5c529e64cf5de8f29c826a86
Ltoro9 <louistororosario@gmail.com>
2024-04-17 21:41:57 -0400

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,40 +4,36 @@
 """
 
 import requests
-from sys import argv
+import sys
 
 
 def get_employee(id=None):
     """
-    using this REST API, for a given employee ID,
-    returns information about his/her TODO list progress.
+    Retrieve and display the TODO list progress for a given employee ID.
     """
     # check if argv[1] is a number int
-    if len(argv) > 1:
+    if len(sys.argv) > 1:
         try:
-            id = int(argv[1])
+            id = int(sys.argv[1])
         except ValueError:
             return
 
     if isinstance(id, int):
         base = "https://jsonplaceholder.typicode.com"
-        user = requests.get(f"{base}/users/{id}", timeout=10).json()
-        to_dos = requests.get(f"{base}/todos/?userId={id}", timeout=10).json()
+        user = requests.get(f"{base}/users/{id}").json()
+        to_dos = requests.get(f"{base}/todos/?userId={id}").json()
 
         if user and to_dos:
             total_tasks = len(to_dos)
-            # fmt: off
+
             titles_completed = [task["title"]
                                 for task in to_dos
                                 if task["completed"]]
-            # fmt: on
+
             tasks_completed = len(titles_completed)
 
-            print(
-                "Employee {} is done with tasks({}/{}):".format(
-                    user["name"], tasks_completed, total_tasks
-                )
-            )
+            print(f"Employee {user['name']} is done with tasks\
+                    ({len(tasks_completed)}/{total_tasks}):")
 
             for title in titles_completed:
                 print(f"\t {title}")
